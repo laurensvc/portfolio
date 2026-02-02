07201d6a90cc8e97f10ba5de95e2ade8f1524ed9
Laurensvc <laurens.van.c@gmail.com>
2026-02-02 16:50:59 +0100

    navigation

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -8,6 +8,4 @@ export const navItems: NavItem[] = [
   { name: "Over", href: "#about", number: "01" },
   { name: "Ervaring", href: "#experience", number: "02" },
   { name: "Vaardigheden", href: "#skills", number: "03" },
-  { name: "Projecten", href: "#projects", number: "04" },
-  { name: "Contact", href: "#contact", number: "05" },
 ];
